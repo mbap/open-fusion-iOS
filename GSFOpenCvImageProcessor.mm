870ad231faafc29d74f202d9c44d2bb21cb80f1b
Michael Baptist <mbaptist@ucsc.edu>
2014-01-28 12:18:10 -0800

    after much struggle got opencv to work. it wasn’t that hard.......

---
+++ GSFOpenCvImageProcessor.mm
@@ -0,0 +1,38 @@
+//
+//  GSFOpenCvImageProcessor.m
+//  GSFDataCollecter
+//
+//  Created by Michael Baptist on 1/28/14.
+//  Copyright (c) 2014 Michael Baptist - LLNL. All rights reserved.
+//
+
+#import "GSFOpenCvImageProcessor.h"
+
+@interface GSFOpenCvImageProcessor ()
+
+@end
+
+@implementation GSFOpenCvImageProcessor
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
