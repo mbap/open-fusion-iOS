8ed0112eb4c2de8c4a21ffe37c3477646bfbaa46
Michael Baptist <mbaptist@ucsc.edu>
2014-02-12 22:33:52 -0800

    committing google maps stuff, however, the maps doesn’t work?? wat!? this makes no sense

---
+++ GSFDataCollecter/GSFGMapViewController.h
@@ -0,0 +1,14 @@
+//
+//  GSFGMapViewController.h
+//  GSFDataCollecter
+//
+//  Created by Michael Baptist on 2/12/14.
+//  Copyright (c) 2014 Michael Baptist - LLNL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GSFTaggedVCViewController.h"
+
+@interface GSFGMapViewController : GSFTaggedVCViewController
+
+@end
