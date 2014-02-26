747de3bacdbf0cc206052d763a0a1e17f8266290
Michael Baptist <brilliantsquid@dhcp-63-184.cse.ucsc.edu>
2014-02-26 12:30:14 -0800

    basic networking layout started.

---
+++ GSFDataCollecter/GSFTrackerHelper.h
@@ -0,0 +1,13 @@
+//
+//  GSFTrackerHelper.h
+//  GSFDataCollecter
+//
+//  Created by Michael Baptist on 2/26/14.
+//  Copyright (c) 2014 Michael Baptist - LLNL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GSFTrackerHelper : NSObject
+
+@end
