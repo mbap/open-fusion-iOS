4243294fd813f3f1903b917114430ac8c4d249bd
Michael Baptist <brilliantsquid@dhcp-63-184.cse.ucsc.edu>
2014-03-10 08:59:09 -0700

    added images to page controller. code signing not found for some reason restarting Xcode.

--- GSFDataCollecter/GSFSavedDataImageViewController.m
+++ GSFDataCollecter/GSFSavedDataImageViewController.m
@@ -27,6 +27,18 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    UIViewController *vc1 = [[UIViewController alloc] init];
+    UIViewController *vc2 = [[UIViewController alloc] init];
+    UIViewController *vc3 = [[UIViewController alloc] init];
+    [vc1.view addSubview:[[UIImageView alloc] initWithImage:[self.images objectAtIndex:0]]];
+    [vc2.view addSubview:[[UIImageView alloc] initWithImage:[self.images objectAtIndex:1]]];
+    [vc3.view addSubview:[[UIImageView alloc] initWithImage:[self.images objectAtIndex:2]]];
+    NSArray *vcs = [[NSArray alloc] initWithObjects:vc1, vc2, vc3, nil];
+    
+    [self setViewControllers:vcs direction:UIPageViewControllerNavigationDirectionForward animated:YES completion:^(BOOL finished){
+        // do nothing.
+    }];
 }
 
 - (void)didReceiveMemoryWarning
