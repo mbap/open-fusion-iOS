3b89e0012c4f7e771a83f0e21463ec9049db00f4
Michael Baptist <mbaptist@ucsc.edu>
2014-03-14 15:08:43 -0700

    created login page.

---
+++ GSFDataCollecter/GSFLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  GSFLoginViewController.h
+//  GSFDataCollecter
+//
+//  Created by Michael Baptist on 3/13/14.
+//  Copyright (c) 2014 Michael Baptist - LLNL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GSFLoginViewController : UIViewController
+
+@end
