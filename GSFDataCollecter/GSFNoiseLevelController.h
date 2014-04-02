7cbfafbe7e3f603e6654d140b5100b9602f24374
mick31 <mjbennett31@gmail.com>
2014-04-02 12:47:18 -0700

    Added noise level monitoring object and still in middle of updating the IO class to a NSObject subclass from a UIViewController subclass. Still need to:     * Finish writing audio callbacks     * Finish writing audio processing function     * Test alert views work with entire app     * Test IO is working

---
+++ GSFDataCollecter/GSFNoiseLevelController.h
@@ -0,0 +1,13 @@
+//
+//  GSFNoiseLevelController.h
+//  GSFDataCollecter
+//
+//  Created by Mick Bennett on 3/31/14.
+//  Copyright (c) 2014 Michael Baptist - LLNL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GSFNoiseLevelController : NSObject
+
+@end
---
+++ GSFDataCollecter/GSFNoiseLevelController.m
@@ -0,0 +1,13 @@
+//
+//  GSFNoiseLevelController.m
+//  GSFDataCollecter
+//
+//  Created by Mick Bennett on 3/31/14.
+//  Copyright (c) 2014 Michael Baptist - LLNL. All rights reserved.
+//
+
+#import "GSFNoiseLevelController.h"
+
+@implementation GSFNoiseLevelController
+
+@end
