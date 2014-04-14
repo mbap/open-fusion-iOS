6eb279874d384e8524aafb1b8a7fda7d8f5421f9
Michael Baptist <mbaptist@ucsc.edu>
2014-04-13 22:28:29 -0700

    Updated all gps code to use the non deprecated methods. The google maps view now loads straight to the users location. Hopefully this time it doesn’t break again.

--- GSFDataCollecter/GSFGMapViewController.m
+++ GSFDataCollecter/GSFGMapViewController.m
@@ -18,6 +18,7 @@
 @property (nonatomic) NSMutableArray *waypointStrings;
 
 @property (nonatomic) CLLocation *bestEffort;
+@property (nonatomic) NSMutableArray *locationMeasurements;
 
 // sets the camera of the map.
 - (void)setGoogleMapCameraLocation:(CLLocation*)location;
@@ -34,7 +35,7 @@
     // get the current user location.
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
-    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
     [self.locationManager startUpdatingLocation];
     
     // Create a GMSCameraPosition that tells the map to display the
@@ -89,37 +90,40 @@
 
 }
 
+
 // delegate for super class location manager
 // gets called several times while the location manager is update gps coords.
-- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
-    
-    NSLog(@"%@", locations.description);
-    
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    // store all of the measurements, just so we can see what kind of data we might receive
     CLLocation *newLocation = [locations lastObject];
-    self.bestEffort = newLocation;
+    [self.locationMeasurements addObject:newLocation];
     
+    // test the age of the location measurement to determine if the measurement is cached
+    // in most cases you will not want to rely on cached measurements
     NSTimeInterval locationAge = -[newLocation.timestamp timeIntervalSinceNow];
     
-    if (locationAge > 5.0) {
-        return;
-    }
-    
+    if (locationAge > 5.0) return;
     // test that the horizontal accuracy does not indicate an invalid measurement
-    if (newLocation.horizontalAccuracy < 0 || newLocation.verticalAccuracy < 0) {
-        return;
-    }
     
+    if (newLocation.horizontalAccuracy < 0) return;
+    // test the measurement to see if it is more accurate than the previous measurement
     
-    // test the measurement to see if it meets the desired accuracy
-    //
-    // IMPORTANT!!! kCLLocationAccuracyBest should not be used for comparison with location coordinate or altitidue
-    // accuracy because it is a negative value. Instead, compare against some predetermined "real" measure of
-    // acceptable accuracy, or depend on the timeout to stop updating. This sample depends on the timeout.
-    //
-    NSLog(@"%f, %f", newLocation.horizontalAccuracy, self.locationManager.desiredAccuracy);
-    if (newLocation.horizontalAccuracy <= self.locationManager.desiredAccuracy) {
-        [self.locationManager stopUpdatingLocation];
+    if (self.bestEffort == nil || self.bestEffort.horizontalAccuracy > newLocation.horizontalAccuracy) {
+        // store the location as the "best effort"
+        self.bestEffort = newLocation;
         [self setGoogleMapCameraLocation:newLocation];
+
+        // test the measurement to see if it meets the desired accuracy
+        //
+        // IMPORTANT!!! kCLLocationAccuracyBest should not be used for comparison with location coordinate or altitidue
+        // accuracy because it is a negative value. Instead, compare against some predetermined "real" measure of
+        // acceptable accuracy, or depend on the timeout to stop updating. This sample depends on the timeout.
+        //
+        if (newLocation.horizontalAccuracy <= self.locationManager.desiredAccuracy) {
+            [self.locationManager stopUpdatingLocation];
+            [self setGoogleMapCameraLocation:newLocation];
+        }
     }
 }
 
