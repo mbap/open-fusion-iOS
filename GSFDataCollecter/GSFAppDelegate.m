2c4d38236557c0f9f27d97b0d5df5b6cd858356b
Michael Baptist <brilliantsquid@dhcp-63-184.cse.ucsc.edu>
2014-04-28 12:40:58 -0700

    added a cfbundleid so we can launch the app from safari

--- GSFDataCollecter/GSFAppDelegate.m
+++ GSFDataCollecter/GSFAppDelegate.m
@@ -40,6 +40,12 @@
     return YES;
 }
 
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    NSLog(@"%@\n%@\n%@\n%@\n", application, url, sourceApplication, annotation);
+    return YES;
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
